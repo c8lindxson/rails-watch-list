5530ee897a2ff4d250d9ce05832f1ee7d7d5bf6a
Cai Dixon <city.art.girl@gmail.com>
2023-05-12 16:09:35 +0900

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
+Bookmark.destroy_all
 Movie.destroy_all
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-
 Movie.create(title: "Singin' in the Rain", overview: "A lighthearted depiction of Hollywood in the late 1920s, with three stars portraying performers caught up in transition from silent films to 'talkies'", poster_url: "https://en.wikipedia.org/wiki/Singin%27_in_the_Rain#/media/File:Singin'_in_the_Rain_(1952_poster).jpg", rating: 8.9)
 Movie.create(title: "Mama Mia", overview: "The story of a bride-to-be trying to find her real father told using hit songs be the popular 1970s group ABBA ", poster_url: "https://en.wikipedia.org/wiki/Mamma_Mia!_(film)#/media/File:MammaMiaTeaserPoster.JPG", rating: 7.3)
 Movie.create(title: "Turning Red", overview: "A 13-year-old girl named Meilin turns into a giant red panda whenever she gets too excited", poster_url: "https://en.wikipedia.org/wiki/Turning_Red#/media/File:Turning_Red_poster.jpg", rating: 9.2)
